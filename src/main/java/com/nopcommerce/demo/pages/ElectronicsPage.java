aa4a352b07d61df2fc8ca997f776c9ea48f57de0
reepat2023 <reepatel2023@gmail.com>
2023-07-11 05:29:44 +0530

    Initial commit

---
+++ src/main/java/com/nopcommerce/demo/pages/ElectronicsPage.java
@@ -0,0 +1,17 @@
+package com.nopcommerce.demo.pages;
+
+import com.nopcommerce.demo.utilities.Utility;
+import org.openqa.selenium.By;
+
+public class ElectronicsPage extends Utility {
+    By electronicsTabText = By.xpath("//h1[normalize-space()='Electronics']");
+    public String getElectronicsHeaderText(){
+        return getTextFromElement(electronicsTabText);
+    }
+
+    public void selectMenu(String menu) {
+        clickOnElement(By.xpath("//ul[@class='top-menu notmobile']//a[normalize-space()='" + menu + "']"));
+    }
+
+}
+
---
+++ src/test/java/com/nopcommerce/demo/testsuite/TopMenuTest.java
@@ -0,0 +1,25 @@
+package com.nopcommerce.demo.testsuite;
+
+import com.nopcommerce.demo.pages.BuildComputerPage;
+import com.nopcommerce.demo.pages.ElectronicsPage;
+import com.nopcommerce.demo.pages.HomePage;
+import com.nopcommerce.demo.testbase.BaseTest;
+import junit.framework.Assert;
+import org.testng.annotations.Test;
+
+public class TopMenuTest extends BaseTest {
+
+    ElectronicsPage electronicsPage = new ElectronicsPage();
+    HomePage homePage = new HomePage();
+
+
+    @Test
+    public void verifyPageNavigation() {
+        String menuName = "Electronics";
+        String expectedMessage = "Electronics";
+        // This method select Electronics tab on top menu
+        electronicsPage.selectMenu(menuName);
+        String actualMessage= electronicsPage.getElectronicsHeaderText();
+        Assert.assertEquals(expectedMessage,actualMessage,"Unable to click on");
+    }
+}
